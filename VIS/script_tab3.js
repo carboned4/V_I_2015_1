9ee55fccfff5eb70ad97805a1a5366cf43581bfe
carboned4 <slf450x@hotmail.com>
2015-11-22 01:39:25 +0000

    tab3 com zoom deficiente

--- VIS/script_tab3.js
+++ VIS/script_tab3.js
@@ -242,6 +242,17 @@ function gen_bubbles() {
 	                   })
 		.attr("fill","white");
 	
+	var zoom = d3.behavior.zoom()
+    .on("zoom",function() {
+        g.attr("transform","translate("+ 
+            d3.event.translate.join(",")+")scale("+d3.event.scale+")");
+        g.selectAll("circle")
+            .attr("d", path.projection(projection));
+        g.selectAll("path")  
+            .attr("d", path.projection(projection)); 
+
+  });
+	
 	
 	//exit?
 	svg.call(zoom);
